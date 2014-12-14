7451295fe0d13a4aa29d5058c6faad7e4b602d2f
Jam-G <cruijunguo@gmail.com>
2014-12-14 23:18:22 +0800

    do some for msg

--- Sever/net/Message/Message.java
+++ Sever/net/Message/Message.java
@@ -2,6 +2,11 @@ package net.Message;
 
 import java.io.Serializable;
 
+import java.awt.Image;
+import java.io.Serializable;
+import java.util.ArrayList;
+import word.Word;
+
 public class Message implements Serializable{
 	public int id;
 	public boolean reply;
@@ -19,4 +24,102 @@ public class Message implements Serializable{
 	public static final int SEND_CARD = 11;
 	public static final int SEND_MESSAGE = 12;
 	public MsgData data;
+	
+
+
+
+
+
+
+	/*
+		static final int LOGIN = 1;
+		static final int LOGOUT = 2;
+		static final int REGISTER = 3;
+		static final int CHANGE_PSW = 4;
+		static final int ADD_FRIEND = 5;
+		static final int DEL_FRIEND = 6;
+		static final int UPDATE_FRIEND_ONLINE = 7;
+		static final int SEARCH = 8;
+		static final int ADD_PRAISE = 9;
+		static final int DEL_PRAISE = 10;
+		static final int SEND_CARD = 11;
+		static final int SEND_MESSAGE = 12;
+	*/
+	public abstract class MsgData implements Serializable{
+	}
+
+	public class LoginMsg extends MsgData implements Serializable{
+		public String uid;
+		public String psw;
+	}
+
+	public class LogoutMsg extends MsgData implements Serializable{
+		public String uid;
+		public String psw;
+	}
+
+	public class RegsiterMsg extends MsgData implements Serializable{
+		public String uid;
+		public String psw;
+		public String email;
+		public char sex;
+	}
+
+	public class ChangePsw extends MsgData implements Serializable{
+		public String uid;
+		public String oldpsw;
+		public String newpsw;
+	}
+
+	public class AddFriend extends MsgData implements Serializable{
+		public String uid;
+		public String psw;
+		public String friend_uid;
+	}
+
+	public class DelFriend extends MsgData implements Serializable{
+		public String uid;
+		public String psw;
+		public String friend_uid;
+	}
+
+	public class OnlineFriend extends MsgData implements Serializable{
+		public String uid;
+		public String psw;
+		public ArrayList<String> friendList;
+	}
+
+	public class Serach extends MsgData implements Serializable{
+		public Word word;
+	}
+
+	public class Add_Praise extends MsgData implements Serializable{
+		public String uid;
+		public String psw;
+		public Word word;
+	}
+
+	public class Del_Praise extends MsgData implements Serializable{
+		public String uid;
+		public String psw;
+		public Word word;
+	}
+
+	public class Send_Card extends MsgData implements Serializable{
+		public String uid;
+		public String psw;
+		public String targetuid;
+		public Image card;
+	}
+
+	public class Send_Message extends MsgData implements Serializable{
+		public String uid;
+		public String psw;
+		public String targetuid;
+		public String dialoge;
+	}
+	public class ReplyData extends MsgData implements Serializable{
+		public boolean success;
+	}
+
 }
