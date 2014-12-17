a086e82d28c7a20caa3e9b9de6942b1e50d1725f
Jam-G <cruijunguo@gmail.com>
2014-12-18 03:40:24 +0800

    add client

--- Client/dict/net/Client.java
+++ Client/dict/net/Client.java
@@ -8,7 +8,7 @@ import java.util.Map;
 
 import net.Message.Message;
 
-public class test {
+public class Client {
 	public static LinkToServer getLink(){
 		Map requests = new HashMap<Integer, Message >();
 		Socket socket = null;
@@ -31,27 +31,18 @@ public class test {
 			
 		}
 		LinkToServer link = new LinkToServer(requests);
-		System.out.println("create link~~~~");
-		ListenMessage msgs = new ListenMessage(requests, socket);
-		System.out.println("create listenner~~~~");
-		ListenCard card = new ListenCard();
-		System.out.println("create cara listenner~~~~");
-		Thread tmsg = new Thread(msgs);
-		tmsg.start();
-		System.out.println("create msg recevice thread~~~~");
-		Thread tcard = new Thread(card);
-		System.out.println("create card recevice thread~~~~");
 		
-		System.out.println("start msg recevice thread~~~~");
+		ListenMessage msgs = new ListenMessage(requests, socket);
+		
+		ListenCard card = new ListenCard();
+	
+		Thread tmsg = new Thread(msgs);
+		
+		
+		Thread tcard = new Thread(card);
+		tmsg.start();
 		tcard.start();
-		System.out.println("start card recevice thread~~~~");
-
-		System.out.println(link.register("jam", "guoruijun", "cruijunguo@gmail.com", 'm'));
-		link.setUid("jam");
-		link.setPsw("guoruijun");
-		System.out.println(link.login());
-		System.out.println(link.addFriend("zhangruiyi"));
-		System.out.println(link.serach("jam"));
+	
 		return link;
 	}
 }
