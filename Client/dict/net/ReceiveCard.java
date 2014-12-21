6a34dbf14532942b1895fcf165af90ebe88b8e7d
zhangry868 <zhangry868@126.com>
2014-12-21 19:45:46 +0800

    Fix Bug

--- Client/dict/net/ReceiveCard.java
+++ Client/dict/net/ReceiveCard.java
@@ -11,7 +11,9 @@ import javax.imageio.ImageIO;
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
 
+import wordcard.WordCard;
 import net.Message.Message;
 
 public class ReceiveCard implements Runnable{
@@ -35,6 +37,10 @@ public class ReceiveCard implements Runnable{
 			synchronized(msgBox){
 				msgBox.add(msg);
 			}
+			WordCard wCard = new WordCard(((Message.Send_Card)msg.data).card);
+			wCard.display();
+			JOptionPane.showMessageDialog(null,
+				       "接收成功", "系统信息", JOptionPane.ERROR_MESSAGE);
 			DefaultListModel<Message> dlist = new DefaultListModel<Message>();
 			dlist.removeAllElements();
 			synchronized(msgBox){
