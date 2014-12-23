8362a5b0c873ef7a42c7d5b5bc41a5a5493092dd
Hackhao <hh@smail.nju.edu.cn>
2014-12-23 17:31:50 +0800

    fix

--- Client/dict/pro/friendInfo.java
+++ Client/dict/pro/friendInfo.java
@@ -6,6 +6,7 @@ import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
